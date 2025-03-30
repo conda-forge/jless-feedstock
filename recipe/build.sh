8df760513380e39280cc93e453a39a34beee796c
Antônio Camargo <apcamargo@users.noreply.github.com>
2025-03-30 12:24:08 -0700

    Add Windows build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -ex
+set -o xtrace -o nounset -o pipefail -o errexit
+
+export CARGO_PROFILE_RELEASE_STRIP=symbols
+export CARGO_PROFILE_RELEASE_LTO=fat
+
+# build statically linked binary with Rust
+cargo install --no-track --locked --root "$PREFIX" --path .
 
 cargo-bundle-licenses \
     --format yaml \
     --output THIRDPARTY.yml
-
-cargo install --locked --root "$PREFIX" --path .
-
-
-# strip debug symbols
-"$STRIP" "$PREFIX/bin/jless"
-
-# remove extra build file
-rm -f "${PREFIX}/.crates.toml"
